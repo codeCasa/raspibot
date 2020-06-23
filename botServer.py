bfe159f2317559531a31490b56ef69149fa5f784
Tre House <trejon_house@yahoo.com>
2020-06-23 03:58:50 +0100

    wip

--- botServer.py
+++ botServer.py
@@ -4,14 +4,36 @@ from PyBot import PyBot
 class PyBotServer:
 	def __init__(self):
 		self.bot = PyBot()
-	async def handleConnection(reader, writer):
-		data = await reader.read(1024)
-		message = data.decode()
-		print("from client %s", message)
+	async def handleConnection(self, reader, writer):
+		while True:
+			try:
+				data = await reader.readline()
+				if not data:
+					continue
+				message = data.decode()
+				print(message)
+				if message == 'forward':
+					self.bot.forward()
+				elif message == 'left':
+					self.bot.left()
+				elif message == 'backward':
+					self.bot.backward()
+				elif message == 'right':
+					self.bot.right()
+				elif message == 'stop':
+					self.bot.stop()
+				elif message == 'disconnect':
+					writer.close()
+					self.bot.stop()
+					break
+			except KeyboardInterrupt:
+				writer.close()
+				break
 
 	async def start(self):
+		print("wating...")
 		server = await asyncio.start_server(
-			handleConnection, '0.0.0.0', 9000)
+			self.handleConnection, '0.0.0.0', 9000)
 
 		addr = server.sockets[0].getsockname()
 		print(f'Serving on {addr}')
